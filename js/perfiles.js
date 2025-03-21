7998b67a56df9c35a800c855cabc42b8ba1059cf
vporrasg <53625582+vporrasg@users.noreply.github.com>
2025-03-21 00:37:24 -0600

    test

--- js/perfiles.js
+++ js/perfiles.js
@@ -25,12 +25,29 @@ function cargarUsuarios() {
                     <p>Dueño: ${usuario.duenno}</p>
                     <p>Contacto: ${usuario.contacto1[0]}${usuario.contacto1[1]}${usuario.contacto1[2]}${usuario.contacto1[3]}-${usuario.contacto1[4]}${usuario.contacto1[5]}${usuario.contacto1[6]}${usuario.contacto1[7]}</p>    
                 `;
+                
 
+                const imgw = document.createElement('img');
+                imgw.src = "../images/w.png"
+
+
+                //<i class="fa fa-arrow-left"></i>
              
+                const btn1 = document.createElement('button');
+                btn1.classList = 'btn-contact';
+                btn1.innerHTML+=`<i class="fa-brands fa-whatsapp"></i> Contactar`;
+                btn1.setAttribute('href', "https://api.whatsapp.com/send/?phone="+usuario.contacto1)
+                btn1.href = "https://api.whatsapp.com/send/?phone="+usuario.contacto1;
+             
+                
+
+
+
                 main.appendChild(card);
-                card.appendChild(imgBorder)
+                card.appendChild(imgBorder) 
                 imgBorder.appendChild(img)
                 card.appendChild(infoBorder)
+                infoBorder.appendChild(btn1)
          
             });
         }) // Aquí mostramos dicha información
